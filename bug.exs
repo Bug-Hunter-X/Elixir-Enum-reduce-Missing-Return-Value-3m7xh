2b6b0fcc0ce19057872de7c472a068592fafcb79
BugHunter-X <askzenai@gmail.com>
2025-01-17 17:36:04 +0100

    Add bug.exs file

---
+++ bug.exs
@@ -0,0 +1,14 @@
+```elixir
+list = [1, 2, 3, 4, 5]
+
+Enum.reduce(list, 0, fn x, acc ->
+  if rem(x, 2) == 0 do
+    acc + x
+  else
+    acc
+  end
+  # Missing return statement here!
+  # The function should explicitly return the accumulator
+  # Without it the value of acc might be undefined.
+end)
+```
